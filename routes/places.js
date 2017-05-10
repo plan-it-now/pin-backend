a1922f99328641aa4e58739fb6b6e193f0f123df
zidane9 <d4rr@hotmail.com>
2017-05-10 17:53:34 +0700

    endpoint places

--- routes/places.js
+++ routes/places.js
@@ -1,2 +1,28 @@
+'use strict'
+
 var express = require('express');
 var router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res) => {
+  res.send([]);
+});
+
+router.get('/:id', (req,res) => {
+  res.send({place:'yuhuy'})
+})
+
+router.post('/', (req,res) => {
+  res.send('success post user');
+})
+
+router.put('/', (req,res) => {
+  res.send('success update place');
+})
+
+router.delete('/:id' (req,res) => {
+  res.send('succes delete place');
+})
+
+
+module.exports = router;
