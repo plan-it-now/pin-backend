e591021e5d596be6440a6a89cfb6d66ed3fcf801
anthonyjuan <anthonyjuan95@gmail.com>
2017-05-11 16:30:32 +0700

    moving to master

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
+var cors = require('cors');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var places = require('./routes/places');
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 const db_config = {
     test: "mongodb://localhost/planitnow-test",
     development:"mongodb://localhost/planitnow",
