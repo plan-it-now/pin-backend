381324f1fbcb4ed385796e526d0540eddadf162e
zidane9 <d4rr@hotmail.com>
2017-05-11 20:46:33 +0700

    userController user.findOne

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,8 +9,11 @@ require('dotenv').config();
 module.exports = {
   login: (req, res) => {
     User.findOne({'email':req.body.email}, (err, user) => {
-      if(err || user == null) {
+      if(err) {
         res.send({error:err})
+      }
+      else if(!user){
+        res.send({error:"user not found"})
       } else {
         if(pwh.verify(req.body.password,user.password)) {
           const newToken = jwt.sign({email: user.email}, process.env.SECRET_KEY);
